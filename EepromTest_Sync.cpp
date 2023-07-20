92d47b39ac32febf2ae9d3dc7985c082a6abf83d
LSG\joseph.selvaraj <joseph.selvaraj@stengg.com>
2023-07-20 16:51:37 +0800

    print messages

--- EepromTest_Sync.cpp
+++ EepromTest_Sync.cpp
@@ -1,46 +1,45 @@
-/** @file HL_sys_main.c 
-*   @brief Application main file
-*   @date 11-Dec-2018
-*   @version 04.07.01
-*
-*   This file contains an empty main function,
-*   which can be used for the application.
-*/
+/** @file HL_sys_main.c
+ *   @brief Application main file
+ *   @date 11-Dec-2018
+ *   @version 04.07.01
+ *
+ *   This file contains an empty main function,
+ *   which can be used for the application.
+ */
 
-/* 
-* Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com  
-* 
-* 
-*  Redistribution and use in source and binary forms, with or without 
-*  modification, are permitted provided that the following conditions 
-*  are met:
-*
-*    Redistributions of source code must retain the above copyright 
-*    notice, this list of conditions and the following disclaimer.
-*
-*    Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the 
-*    documentation and/or other materials provided with the   
-*    distribution.
-*
-*    Neither the name of Texas Instruments Incorporated nor the names of
-*    its contributors may be used to endorse or promote products derived
-*    from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*/
-
+/*
+ * Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 /* USER CODE BEGIN (0) */
 
@@ -84,36 +83,37 @@
 /* USER CODE END */
 
 /** @fn void main(void)
-*   @brief Application main function
-*   @note This function is empty by default.
-*
-*   This function is called after startup.
-*   The user can use this function to implement the application.
-*/
+ *   @brief Application main function
+ *   @note This function is empty by default.
+ *
+ *   This function is called after startup.
+ *   The user can use this function to implement the application.
+ */
 
 /* USER CODE BEGIN (2) */
 #define UART sciREG1
 
-#define  TSIZE1 10
-uint8  TEXT1[TSIZE1]= {'H','E','R','C','U','L','E','S',' ',' '};
-#define  TSIZE2 18
-uint8  TEXT2[TSIZE2]= {'M','I','C','R','O','C','O','N','T','R','O','L','L','E','R','S',' ',' '};
-#define  TSIZE3 19
-uint8  TEXT3[TSIZE3]= {'T','E','X','A','S',' ','I','N','S','T','R','U','M','E','N','T','S','\n','\r'};
-#define SPACESIZE 1
-uint8  SPACE[SPACESIZE]= {' '};
-#define BREAKSIZE 2
-uint8  BREAK[BREAKSIZE]= {'\n','\r'};
+#define TSIZE3 19
+uint8 TEXT3[TSIZE3] = {'T', 'E', 'X', 'A', 'S', ' ', 'I', 'N', 'S', 'T', 'R', 'U', 'M', 'E', 'N', 'T', 'S', '\n', '\r'};
 
-const int mainCounterAddressOffset = 0U;
+#define SPACESIZE 1
+uint8 SPACE[SPACESIZE] = {' '};
+#define BREAKSIZE 2
+uint8 BREAK[BREAKSIZE] = {'\n', '\r'};
+#define TSIZE1 23
+uint8 TEXT1[TSIZE1] = {'R', 'E', 'T', 'R', 'I', 'E', 'V', 'E,' D ',' ',' C ',' O ',' U',' N ',' T ',' E ',' R ',' O ',' N ',' E ',' : ',' ',' '};
+#define TSIZE2 23
+                                                          uint8 TEXT2[TSIZE2] = {'R', 'E', 'T', 'R', 'I', 'E', 'V', 'E,' D ',' ',' C ',' O ',' U',' N ',' T ',' E ',' R ',' T ',' W ',' O ',' : ',' ',' '};
+
+                                                                                                                    const int mainCounterAddressOffset = 0U;
 const int mainCountersAddress = 0x01;
 const int mainCountersTotalByteSize = 16U;
 
 int EepromSaveInterval = 60000;     // Every 60 seconds
 int CouterIncrementInterval = 1000; // Every 1 second
-int flush = 0;
-uint32_t mainCounterOne = 0U;
-uint32_t mainCounterTwo = 1U;
+uint32_t flush = 0;
+uint32_t mainCounterOne = 0;
+uint32_t mainCounterTwo = 1;
 volatile bool saveEepromFlag = false;
 volatile bool incrementCounterFlag = false;
 volatile int eepromTimerCounter = 0U;
@@ -121,7 +121,7 @@ volatile int incrementTimerCounter = 0U;
 
 void loadCountersFromEEPROM(void)
 {
-    uint8_t loadBufferArray[16];
+    uint8_t loadBufferArray[16] = {0};
     // Loads both main counters from same block address
     TI_Fee_ReadSync(mainCountersAddress, mainCounterAddressOffset, (uint8_t *)loadBufferArray, mainCountersTotalByteSize);
     memcpy(&mainCounterOne, loadBufferArray, 4U);     // Extract first 4 bytes for mainCounterOne
@@ -144,26 +144,29 @@ void flushEEPROM(void)
     TI_Fee_WriteSync(mainCountersAddress, (uint8_t *)writeBufferArray);
 }
 
-void sciPrintDecimal(sciBASE_t *sci, uint32_t value) {
+void sciPrintDecimal(sciBASE_t *sci, uint32_t value)
+{
     char buffer[11]; // Buffer to hold the string representation of the decimal value
     int i = 0;
     snprintf(buffer, 11, "%lu", value); // Convert the uint32 value to a string
 
     // Print each character of the string until the null terminator
-    for(i = 0; buffer[i] != '\0'; i++)
+    for (i = 0; buffer[i] != '\0'; i++)
     {
-        while ((sci->FLR & 0x4) == 4); // Wait until busy
+        while ((sci->FLR & 0x4) == 4)
+            ;                        // Wait until busy
         sciSendByte(sci, buffer[i]); // Send out text
     }
     i = 0;
 }
 
-void sciDisplayText(sciBASE_t *sci, uint8 *text,uint32 length)
+void sciDisplayText(sciBASE_t *sci, uint8 *text, uint32 length)
 {
-    while(length--)
+    while (length--)
     {
-        while ((UART->FLR & 0x4) == 4); /* wait until busy */
-        sciSendByte(UART,*text++);      /* send out text   */
+        while ((UART->FLR & 0x4) == 4)
+            ;                       /* wait until busy */
+        sciSendByte(UART, *text++); /* send out text   */
     };
 }
 
@@ -175,21 +178,20 @@ void wait(uint32 time)
 
 int main(void)
 {
-/* USER CODE BEGIN (3) */
+    /* USER CODE BEGIN (3) */
 
     TI_Fee_Init();
 
     /* Initialize RTI driver */
     rtiInit();
 
-    sciInit();      /* initialize sci/sci-lin    */
-                        /* even parity , 2 stop bits */
+    sciInit(); /* initialize sci/sci-lin    */
+               /* even parity , 2 stop bits */
 
     /* Set high end timer GIO port hetPort pin direction to all output */
     gioSetDirection(hetPORT1, 0xFFFFFFFF);
 
-
-    //flushEEPROM();
+    // flushEEPROM();
     loadCountersFromEEPROM();
 
     /* Enable RTI Compare 0 interrupt notification */
@@ -200,9 +202,10 @@ int main(void)
     /* Start RTI Counter Block 0 */
     rtiStartCounter(rtiREG1, rtiCOUNTER_BLOCK0);
 
-
+    sciDisplayText(UART, &TEXT1[0], TSIZE1); /* send text code 3 */
     sciPrintDecimal(UART, mainCounterOne);
-    sciDisplayText(UART,&BREAK[0],BREAKSIZE);   /* send text code 3 */
+    sciDisplayText(UART, &BREAK[0], BREAKSIZE); /* send text code 3 */
+    sciDisplayText(UART, &TEXT2[0], TSIZE2);    /* send text code 3 */
     sciPrintDecimal(UART, mainCounterTwo);
 
     wait(200);
@@ -229,7 +232,6 @@ int main(void)
 
     return 0;
 }
-
 
 /* USER CODE BEGIN (4) */
 

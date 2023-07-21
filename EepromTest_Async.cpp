bd187d69d1ee6b0073182d400b440c1d1568a647
LSG\xuanjun.nigel.chan <xuanjun.nigel.chan@stengg.com>
2023-07-21 11:39:35 +0800

    async

--- EepromTest_Async.cpp
+++ EepromTest_Async.cpp
@@ -1,95 +1,261 @@
+/** @file HL_sys_main.c
+ *   @brief Application main file
+ *   @date 11-Dec-2018
+ *   @version 04.07.01
+ *
+ *   This file contains an empty main function,
+ *   which can be used for the application.
+ */
+
 /*
- *  fee.c
- *  Handling EEPROM write & read data
- *  Created on: 06 July, 2022
- *      Author: Lukman
+ * Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  */
 
 
-#include "fee.h"
-#include "rotary.h"
+/* USER CODE BEGIN (0) */
+/*********************************************FLOW OF EVENTS*********************************************/
+// To test the writing to EEPROM and then loading back the correct values via "Serial Monitor"
+// 1. Load counters from EEPROM
+// 2. Start RTI timer
+// 3. Every 1 second, increment counters
+// 4. Every 60 seconds, save counters to EEPROM
+/********************************************************************************************************/
+/* USER CODE END */
+
+/* Include Files */
+
+#include "HL_sys_common.h"
+
+/* USER CODE BEGIN (1) */
+#include "HL_sys_core.h"
+#include "HL_mibspi.h"
+#include "HL_esm.h"
+#include "HL_rti.h"
+#include "HL_gio.h"
+#include "HL_het.h"
+#include "ti_fee.h"
 #include "string.h"
 #include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+#include "HL_system.h"
+#include "HL_sci.h"
+
+#define  SAVEDSIZE 6
+uint8  SAVED[SAVEDSIZE]= {'S','A','V','E','D',' '};
+#define SPACESIZE 1
+uint8  SPACE[SPACESIZE]= {' '};
+#define BREAKSIZE 2
+uint8  BREAK[BREAKSIZE]= {'\n','\r'};
+
+/* USER CODE END */
+
+/** @fn void main(void)
+ *   @brief Application main function
+ *   @note This function is empty by default.
+ *
+ *   This function is called after startup.
+ *   The user can use this function to implement the application.
+ */
+
+/* USER CODE BEGIN (2) */
+#define UART sciREG1
+uint16 u16JobResult,Status;
 
 
+const int mainCounterAddressOffset = 0U;
+const int mainCountersAddress = 0x01;
+const int mainCountersTotalByteSize = 8U;
 
-void EEPROM_readValue(uint8_t* buffer_o, unsigned int length, uint8_t block_number)
+int EepromSaveInterval = 20000;     // Every 20 seconds
+int CouterIncrementInterval = 1000; // Every 1 second
+int mainCounterOne = 30U;
+int mainCounterTwo = 31U;
+volatile bool saveEepromFlag = false;
+volatile bool incrementCounterFlag = false;
+volatile int eepromTimerCounter = 0U;
+volatile int incrementTimerCounter = 0U;
+
+void sciDisplayText(sciBASE_t *sci, uint8 *text,uint32 length);
+void sciPrintDecimal(sciBASE_t *sci, uint32_t value);
+
+void loadCountersFromEEPROM(void)
 {
-    uint16_t Status;
+    uint8_t loadBufferArray[16];
+    // Loads both main counters from same block address
+    TI_Fee_ReadSync(mainCountersAddress, mainCounterAddressOffset, (uint8_t *)loadBufferArray, mainCountersTotalByteSize);
+    memcpy(&mainCounterOne, loadBufferArray, 4U);     // Extract first 4 bytes for mainCounterOne
+    memcpy(&mainCounterTwo, loadBufferArray + 4, 4U); // Extract next 4 bytes for mainCounterTwo
+}
 
-    TI_Fee_Read(block_number, 0, buffer_o, length);
-    //TI_Fee_Read(block_number, 0, buffer_o, 0xFFFF);
+void delay(void)
+{
+    unsigned int dummycnt=0x0000FFU;
+    do
+    {
+        dummycnt--;
+    }
+    while(dummycnt>0);
+}
+
+void wait(uint32 time)
+{
+    time--;
+}
+
+void saveCountersToEEPROM(void)
+{
+    uint8_t writeBufferArray[16];
+    memcpy(writeBufferArray, &mainCounterOne, 4U);
+    memcpy(writeBufferArray + 4, &mainCounterTwo, 4U);
+    TI_Fee_WriteAsync(mainCountersAddress, (uint8_t *)writeBufferArray);
     do
     {
         TI_Fee_MainFunction();
-        delay(0xFF);
+        delay();
         Status=TI_Fee_GetStatus(0);
     }
-   while(Status!=IDLE);
+    while(Status!=IDLE);
 
 }
+/* USER CODE END */
 
-void EEPROM_readData(unsigned int address)
+void main(void)
 {
+    /* USER CODE BEGIN (3) */
 
-  EEPROM_readValue(rd_data, 0xFFFF, address);
-  datfee_rd1= rd_data[1]; //take the last 8bit data  //
-  datfee_rd1=(rd_data[3]<<8)+datfee_rd1;  //add first 8 bit data with the last 8bit data//
+    TI_Fee_Init();
 
-  datfee_rd2= rd_data[5]; //take the last 8bit data  //
-  datfee_rd2=(rd_data[7]<<8)+datfee_rd2;  //add first 8 bit data with the last 8bit data//
+    /* Initialize RTI driver */
+    rtiInit();
 
-}
+    /* initialize sci/sci-lin    */
+    sciInit();
 
-void EEPROM_readCounterData(unsigned int address,uint32_t *pTestCounter, uint32_t *pMotorPos)
-{
+    /* Set high end timer GIO port hetPort pin direction to all output */
+    gioSetDirection(hetPORT1, 0xFFFFFFFF);
 
-  uint16_t Status;
+    loadCountersFromEEPROM();
 
-  TI_Fee_ReadSync(address, 0, (uint8_t*)au8Read_buff, 16U);
-  //TI_Fee_ReadSync(u16EeBlockAdr, 0, pu8DataBuff, u16BlockSize);
-  //EEPROM_readValue(au8Read_buff, 16U, address);
-  memcpy(pTestCounter,au8Read_buff, 4U);
-  memcpy(pMotorPos,  au8Read_buff+4, 4U);
+    /* Enable RTI Compare 0 interrupt notification */
+    rtiEnableNotification(rtiREG1, rtiNOTIFICATION_COMPARE0);
 
-}
+    _enable_IRQ_interrupt_();
 
-void EEPROM_writeValue(uint8_t* buffer_i, uint8_t block_number)
-{
-    uint16_t Status;
+    /* Start RTI Counter Block 0 */
+    rtiStartCounter(rtiREG1, rtiCOUNTER_BLOCK0);
 
-    TI_Fee_WriteSync(block_number, buffer_i);
-    do
+    /* Run forever */
+    while (1)
     {
-        TI_Fee_MainFunction();
-        delay(0xFF);
-        Status=TI_Fee_GetStatus(0);
+        if (incrementCounterFlag)
+        {
+            // Increment counters every 1 sec
+            mainCounterOne++;
+            mainCounterTwo++; // one value higher than mainCounterOne
+            incrementCounterFlag = false;
+            sciPrintDecimal(sciREG1, mainCounterOne);
+            sciDisplayText(sciREG1,&SPACE[0],SPACESIZE);
+            sciPrintDecimal(sciREG1, mainCounterTwo);
+            sciDisplayText(sciREG1,&BREAK[0],BREAKSIZE);
+        }
+
+        if (saveEepromFlag)
+        {
+            // Save counters to EEPROM every 60 sec
+            saveCountersToEEPROM();
+            sciDisplayText(sciREG1,&SAVED[0],SAVEDSIZE);
+            sciPrintDecimal(sciREG1, mainCounterOne);
+            sciDisplayText(sciREG1,&SPACE[0],SPACESIZE);
+            sciPrintDecimal(sciREG1, mainCounterTwo);
+            sciDisplayText(sciREG1,&BREAK[0],BREAKSIZE);
+            wait(200);
+            saveEepromFlag = false;
+        }
     }
-   while(Status!=IDLE);
 
+    /* USER CODE END */
 }
 
-void EEPROM_writeData(uint16_t data1, uint16_t data2, unsigned int address)
+/* USER CODE BEGIN (4) */
+
+/* Note-You need to remove rtiNotification from notification.c to avoid redefinition */
+void rtiNotification(rtiBASE_t *rtiREG, uint32 notification)
 {
+    // Increments every 1ms
+    incrementTimerCounter++;
+    eepromTimerCounter++;
 
-    wr_data[0]= (uint8_t)data1;
-    wr_data[1]= data1>>8;
-	//memcpy(wr_data, &u32data1, 4U);
-    wr_data[2]= (uint8_t)data2;
-    wr_data[3]= data2>>8;
-	//memcpy(wr_data+4, &u32data2, 4U);
-    EEPROM_writeValue((uint8_t*)wr_data, address);
-    delay(100);
+    /***********************1 Second timer*************************/
+    if (incrementTimerCounter >= CouterIncrementInterval)
+    {
+        incrementCounterFlag = true;
+        incrementTimerCounter = 0U;
+    }
+    /**************************************************************/
+
+    /***********************60 Seconds timer***********************/
+    if (eepromTimerCounter >= EepromSaveInterval)
+    {
+        gioSetPort(hetPORT1, gioGetPort(hetPORT1) ^ 0x00000001); // Toggle HNET Pin 0 LED
+        saveEepromFlag = true;
+        eepromTimerCounter = 0U;
+    }
+    /**************************************************************/
 }
 
-void EEPROM_writeCounterData(uint32_t u32data1, uint32_t u32data2, unsigned int address)
+int i = 0;
+
+void sciPrintDecimal(sciBASE_t *sci, uint32_t value)
 {
-
-	memcpy(au8Write_buff, &u32data1, 4U);
-	memcpy(au8Write_buff+4, &u32data2, 4U);
-	TI_Fee_WriteSync(address, (uint8_t*)au8Write_buff);
-	//EEPROM_writeValue((uint8_t*)au8Write_buff,address);
-	delay(100);
+    char buffer[11]; // Buffer to hold the string representation of the decimal value
+    snprintf(buffer, 11, "%lu", value); // Convert the uint32 value to a string
+    // Print each character of the string until the null terminator
+    for(i = 0; buffer[i] != '\0'; i++)
+    {
+        while ((sci->FLR & 0x4) == 4); // Wait until busy
+        sciSendByte(sci, buffer[i]); // Send out text
+    }
+    i = 0;
 }
 
+void sciDisplayText(sciBASE_t *sci, uint8 *text,uint32 length)
+{
+    while(length--)
+    {
+        while ((UART->FLR & 0x4) == 4); /* wait until busy */
+        sciSendByte(UART,*text++);      /* send out text   */
+    };
+}
 
+/* USER CODE END */
